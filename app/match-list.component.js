eb1f1e29bce4e28b6ea5d48e3670dfa7e87ac26a
Bastian Rusche <bastian.rusche@googlemail.com>
2017-10-17 00:21:29 +0200

    save work

--- app/match-list.component.js
+++ app/match-list.component.js
@@ -7,13 +7,16 @@ angular.
     templateUrl: 'match-list.component.html',
     controller: function MatchListController($scope) {
       var ml = this;
-      $scope.match = {};
+      $scope.match = {
+        goalsfor: 0,
+        goalsagainst: 0
+      };
       this.matches = [];
 
       $scope.saveForm = function() {
         
         var newMatch = angular.copy($scope.match);
-        console.log(newMatch);
+        
         ml.matches.push(newMatch);
         $scope.match = {};
         
