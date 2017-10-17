9c76f733f576ffa9bb44cf8879eaed10c3e9870b
Bastian Rusche <bastian@Bastians-MacBook-Pro.local>
2017-10-17 11:10:24 +0200

    goals increment / decrement added, form edited, Match -List table edited

--- app/match-list.component.js
+++ app/match-list.component.js
@@ -15,11 +15,37 @@ angular.
 
       $scope.saveForm = function() {
         
+        //Das aktuelle Match aus dem Form kopieren zuum Persistieren
         var newMatch = angular.copy($scope.match);
+
+        if (!(newMatch.team) || !(newMatch.team)) return;
         
         ml.matches.push(newMatch);
         $scope.match = {};
         
+      }
+
+      $scope.incrementGoals = function(goals, target) {
+        switch (target) {
+          case 'goalsfor': 
+            $scope.match.goalsfor = goals+1;
+            break;
+          case 'goalsagainst':
+            $scope.match.goalsagainst = goals+1;
+            break;
+        }
+      }
+
+      $scope.decrementGoals = function(goals, target) {
+        if (goals == 0) return;
+        switch (target) {
+          case 'goalsfor': 
+            $scope.match.goalsfor = goals-1;
+            break;
+          case 'goalsagainst':
+            $scope.match.goalsagainst = goals-1;
+            break;
+        }
       }
 
     }
